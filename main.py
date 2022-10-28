df7098ad70668db046ea87fcd6dec5cc089f73ce
Diana <dicomeinit@gmail.com>
2022-10-28 18:41:27 +0300

    Rendering Individual Posts

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 import requests
 
 
-posts = requests.get("https://api.npoint.io/e2dc4b70333f013beebe").json()
+posts = requests.get("https://gist.githubusercontent.com/gellowg/389b1e4d6ff8effac71badff67e4d388/raw/fc31e41f8e1a6b713eafb9859f3f7e335939d518/data.json").json()
 
 app = Flask(__name__)
 
@@ -10,6 +10,15 @@ app = Flask(__name__)
 @app.route('/')
 def get_all_posts():
     return render_template("index.html", all_posts=posts)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
 
 
 @app.route('/about')
